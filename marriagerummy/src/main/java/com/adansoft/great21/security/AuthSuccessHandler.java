d8b6624afd3cd51170edb8059fdfd9723e621ee7
arun_nh <arun_nh@yahoo.com>
2014-12-24 13:32:08 -0800

    Adding security lots of things to do.

---
+++ marriagerummy/src/main/java/com/adansoft/great21/security/AuthSuccessHandler.java
@@ -0,0 +1,26 @@
+package com.adansoft.great21.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+public class AuthSuccessHandler implements AuthenticationSuccessHandler {
+
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	
+	public void onAuthenticationSuccess(HttpServletRequest req,
+			HttpServletResponse resp, Authentication auth) throws IOException,
+			ServletException {
+		System.out.println(auth.getPrincipal().getClass());
+		String targetURL = "/RummyPage.jsp";
+		redirectStrategy.sendRedirect(req,resp,targetURL);
+	}
+
+}
