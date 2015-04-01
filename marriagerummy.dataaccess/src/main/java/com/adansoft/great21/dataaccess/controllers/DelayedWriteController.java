33f8bd0f68362f0f1e5a2d9035cf84eff54e337d
arun_nh <arun_nh@yahoo.com>
2015-04-01 16:59:31 -0700

    Delayed write template started

---
+++ marriagerummy.dataaccess/src/main/java/com/adansoft/great21/dataaccess/controllers/DelayedWriteController.java
@@ -0,0 +1,10 @@
+package com.adansoft.great21.dataaccess.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(DataAccessServiceURLs.DATAACCESS_BASE)
+public class DelayedWriteController {
+
+}
